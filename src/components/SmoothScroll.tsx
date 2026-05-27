732bda45f1394bc186acd60ec348ff908791395a
Hunter <viizohh@gmail.com>
2026-05-26 20:12:14 -0400

    Fix TypeScript error: Remove invalid smoothTouch property from Lenis config

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -12,7 +12,6 @@ export default function SmoothScroll() {
       gestureOrientation: 'both',
       smoothWheel: true,
       wheelMultiplier: 1,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     })
