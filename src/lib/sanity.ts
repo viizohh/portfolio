1d5e4ad22a39a4eae96bc32f47302230639d59bb
Hunter <viizohh@gmail.com>
2026-05-26 20:17:34 -0400

    Fix Sanity config: Hardcode projectId instead of env var

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,8 +3,8 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: 'zjdwzsdp',
+  dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
 })
